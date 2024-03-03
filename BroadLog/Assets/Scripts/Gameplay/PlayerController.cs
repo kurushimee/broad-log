b26e54ec0d2c90796a18c951a25c9b60a674a39e
iver0 <ermakov@skiff.com>
2024-03-03 15:22:29 +0700

    Disable diagonal movement

--- BroadLog/Assets/Scripts/Gameplay/PlayerController.cs
+++ BroadLog/Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,41 +1,43 @@
 using System.Collections;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Gameplay
 {
-    [SerializeField] private float moveSpeed;
-    private bool isMoving;
-    private Vector2 moveInput;
-
-    private void Update()
+    public class PlayerController : MonoBehaviour
     {
-        if (!isMoving)
-        {
-            moveInput.x = Input.GetAxisRaw("Horizontal");
-            moveInput.y = Input.GetAxisRaw("Vertical");
+        [SerializeField] private float moveSpeed;
+        private bool _isMoving;
+        private Vector2 _moveInput;
 
-            if (moveInput != Vector2.zero)
+        private void Update()
+        {
+            if (_isMoving) return;
+            _moveInput.x = Input.GetAxisRaw("Horizontal");
+            _moveInput.y = Input.GetAxisRaw("Vertical");
+
+            // Disable diagonal movement
+            if (_moveInput.x != 0) _moveInput.y = 0;
+            
+            if (_moveInput == Vector2.zero) return;
+            var targetPos = transform.position;
+            targetPos.x += _moveInput.x;
+            targetPos.y += _moveInput.y;
+
+            StartCoroutine(Move(targetPos));
+        }
+
+        private IEnumerator Move(Vector3 targetPos)
+        {
+            _isMoving = true;
+
+            while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
             {
-                var targetPos = transform.position;
-                targetPos.x += moveInput.x;
-                targetPos.y += moveInput.y;
-
-                StartCoroutine(Move(targetPos));
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+                yield return null;
             }
+            transform.position = targetPos;
+
+            _isMoving = false;
         }
-    }
-
-    private IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-
-        isMoving = false;
     }
 }
