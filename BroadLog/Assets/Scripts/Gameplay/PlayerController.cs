68c15d0443a6e04d2f5ceb29849ee9762dffa7df
iver0 <ermakov@skiff.com>
2024-03-05 19:58:43 +0700

    Add animations

--- BroadLog/Assets/Scripts/Gameplay/PlayerController.cs
+++ BroadLog/Assets/Scripts/Gameplay/PlayerController.cs
@@ -5,25 +5,44 @@ namespace Gameplay
 {
     public class PlayerController : MonoBehaviour
     {
+        private static readonly int MoveX = Animator.StringToHash("moveX");
+        private static readonly int MoveY = Animator.StringToHash("moveY");
+        private static readonly int IsMoving = Animator.StringToHash("isMoving");
         [SerializeField] private float moveSpeed;
         [SerializeField] private bool diagonalMovement;
+
+        private Animator _animator;
         private bool _isMoving;
         private Vector2 _moveInput;
 
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
         private void Update()
         {
-            if (_isMoving) return;
-            _moveInput.x = Input.GetAxisRaw("Horizontal");
-            _moveInput.y = Input.GetAxisRaw("Vertical");
+            if (!_isMoving)
+            {
+                _moveInput.x = Input.GetAxisRaw("Horizontal");
+                _moveInput.y = Input.GetAxisRaw("Vertical");
 
-            if (!diagonalMovement && _moveInput.x != 0) _moveInput.y = 0;
-            
-            if (_moveInput == Vector2.zero) return;
-            var targetPos = transform.position;
-            targetPos.x += _moveInput.x;
-            targetPos.y += _moveInput.y;
+                if (!diagonalMovement && _moveInput.x != 0) _moveInput.y = 0;
 
-            StartCoroutine(Move(targetPos));
+                if (_moveInput != Vector2.zero)
+                {
+                    _animator.SetFloat(MoveX, _moveInput.x);
+                    _animator.SetFloat(MoveY, _moveInput.y);
+
+                    var targetPos = transform.position;
+                    targetPos.x += _moveInput.x;
+                    targetPos.y += _moveInput.y;
+
+                    StartCoroutine(Move(targetPos));
+                }
+            }
+
+            _animator.SetBool(IsMoving, _isMoving);
         }
 
         private IEnumerator Move(Vector3 targetPos)
@@ -35,6 +54,7 @@ namespace Gameplay
                 transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
                 yield return null;
             }
+
             transform.position = targetPos;
 
             _isMoving = false;
